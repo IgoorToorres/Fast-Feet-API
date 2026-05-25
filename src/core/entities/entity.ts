caac8db7571c0955b19c256cbbc3b0e73aa54f85
IgoorToorres <igorevangelista121@gmail.com>
2026-05-25 09:26:06 -0300

    core - codigo base do projeto

---
+++ src/core/entities/entity.ts
@@ -0,0 +1,21 @@
+import { UniqueEntityID } from './unique-entity-id';
+
+export abstract class Entity<Props> {
+  private _id: UniqueEntityID;
+  protected props: Props;
+
+  protected constructor(props: Props, id?: UniqueEntityID) {
+    this.props = props;
+    this._id = id ?? new UniqueEntityID(id);
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  public equals(entity: Entity<any>) {
+    if (entity === this) return true;
+    if (entity.id === this.id) return true;
+    return false;
+  }
+}
